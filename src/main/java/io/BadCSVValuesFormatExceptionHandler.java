23a1101512a7d27c34bed4c048b7e4ae1f7652a8
Nazarii Ihnat <ignatnazar@gmail.com>
2021-03-23 22:23:41 +0100

    Handler remove values after fifth coma

--- src/main/java/io/BadCSVValuesFormatExceptionHandler.java
+++ src/main/java/io/BadCSVValuesFormatExceptionHandler.java
@@ -5,10 +5,16 @@ import com.opencsv.exceptions.CsvException;
 
 public class BadCSVValuesFormatExceptionHandler implements CsvExceptionHandler {
     @Override
-    public CsvException handleException(CsvException e) throws CsvException {
+    public CsvException handleException(CsvException e){
         String[] values = e.getLine();
-        String[] valuesWithNull = new String[5];
-        System.arraycopy(values, 0, valuesWithNull, 0, values.length);
+        String[] valuesWithNull;
+        if(values.length > 5){
+            valuesWithNull = new String[5];
+            System.arraycopy(values, 0, valuesWithNull, 0, 5);
+        } else {
+            valuesWithNull = new String[values.length];
+            System.arraycopy(values, 0, valuesWithNull, 0, values.length);
+        }
         e.setLine(valuesWithNull);
         e.fillInStackTrace();
         return e;
