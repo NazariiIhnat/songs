7681de7403223d2aab3d84d006e716145d616159
Nazarii Ihnat <ignatnazar@gmail.com>
2021-03-23 22:25:52 +0100

    Created tests for readFromCSV()

--- src/test/java/SongReaderImplTest.java
+++ src/test/java/SongReaderImplTest.java
@@ -11,7 +11,7 @@ import java.util.List;
 public class SongReaderImplTest {
 
     private static SongReader songReader = new SongReaderImpl();
-    private static final String xmlForTestsFolderPath = "src/main/resources/xml/";
+    private static final String xmlForTestsFolderPath = "src/main/resources/test/";
     private StringBuilder stringBuilder = new StringBuilder(xmlForTestsFolderPath);
 
     private File getFile(String fileName){
@@ -69,6 +69,63 @@ public class SongReaderImplTest {
         Song song = songs.get(0);
         int expected = 0;
         int actual = song.getVotes();
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void readFromCSV_CORRECT_ATTRIBUTES() {
+        String fileName = "correct attributes.csv";
+        File file = getFile(fileName);
+        List<Song> songs = songReader.readFromCSV(file);
+        int expected = 3;
+        int actual = songs.size();
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void readFromCSV_MISSING_TITLE() {
+        String fileName = "missing title.csv";
+        File file = getFile(fileName);
+        List<Song> songs = songReader.readFromCSV(file);
+        for(Song song : songs)
+            assertNull(song.getTitle());
+    }
+
+    @Test
+    public void readFromCSV_MISSING_ALL_ATTRIBUTES() {
+        String fileName = "missing all attributes.csv";
+        File file = getFile(fileName);
+        List<Song> songs = songReader.readFromCSV(file);
+        int expected = 0;
+        int actual = songs.size();
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void readFromCSV_INCORRECT_CATEGORY() {
+        String fileName = "incorrect category.csv";
+        File file = getFile(fileName);
+        List<Song> songs = songReader.readFromCSV(file);
+        Song song = songs.get(0);
+        assertNull(song.getCategory());
+    }
+
+    @Test
+    public void readFromCSV_STRING_VOTES() {
+        String fileName = "string votes.csv";
+        File file = getFile(fileName);
+        List<Song> songs = songReader.readFromCSV(file);
+        for(Song song : songs)
+            assertEquals(0, song.getVotes());
+    }
+
+    @Test
+    public void readFromCSV_ADDITIONAL_SEPARATOR() {
+        String fileName = "additional separator.csv";
+        File file = getFile(fileName);
+        List<Song> songs = songReader.readFromCSV(file);
+        int expected = 3;
+        int actual = songs.size();
         assertEquals(expected, actual);
     }
 }
